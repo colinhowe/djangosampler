d043bf6a643e50d9291131879f997f7a71cd90f9
Colin Howe <colinthehowe@gmail.com>
2011-11-26 11:29:44 +0000

    Minor tidy and version bump

--- djangosampler/__init__.py
+++ djangosampler/__init__.py
@@ -1,5 +1,5 @@
 import plugins
 
-VERSION = '0.3.1'
+VERSION = '0.4.0'
 
 plugins.install_plugins()
--- djangosampler/admin.py
+++ djangosampler/admin.py
@@ -6,23 +6,22 @@ from .models import Query, Stack, Sample
 class QueryAdmin(admin.ModelAdmin):
     list_display = ('hash', 'created_dt',)
     list_filter = ('created_dt', 'query_type',)
-    readonly_fields=('created_dt',)
+    readonly_fields = ('created_dt',)
 
 
 class StackAdmin(admin.ModelAdmin):
     list_display = ('hash', 'created_dt',)
     list_filter = ('created_dt',)
-    readonly_fields=('created_dt',)
+    readonly_fields = ('created_dt',)
 
 
 class SampleAdmin(admin.ModelAdmin):
     list_display = ('created_dt',)
     list_filter = ('created_dt',)
-    readonly_fields=('created_dt',)
+    readonly_fields = ('created_dt',)
 
     
 admin.site.register(Query, QueryAdmin)
 admin.site.register(Stack, StackAdmin)
 admin.site.register(Sample, SampleAdmin)
-
 
